ae8464a7656512fbaced11c0c43abb2e152fe8a2
Katerina Kanellopoulou <akanellop42@gmail.com>
2021-01-23 18:27:19 +0200

    complete working autorized branch

--- controllers/user.js
+++ controllers/user.js
@@ -12,6 +12,8 @@ exports.signup = (req, res, next) => {
           password: hash,
           address: req.body.address
         });
+
+
         //cart creation for user
         const cart = new Cart({
           userID:user._id,
@@ -45,39 +47,43 @@ exports.signup = (req, res, next) => {
     );
   };
 
-  exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email }).then(
-      (user) => {
-        if (!user) {
-          return res.status(401).json({
-            error: new Error('User not found!')
-          });
-        }
-        bcrypt.compare(req.body.password, user.password).then(
-          (valid) => {
-            if (!valid) {
-              return res.status(401).json({
-                error: new Error('Incorrect password!')
-              });
-            }
-            res.status(200).json({
-              userId: user._id,
-              token: 'token'
-            });
-          }
-        ).catch(
-          (error) => {
-            res.status(500).json({
-              error: error
-            });
-          }
-        );
-      }
-    ).catch(
-      (error) => {
-        res.status(500).json({
-          error: error
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email }).then(
+    (user) => {
+      if (!user) {
+        return res.status(401).json({
+          error: new Error('User not found!')
         });
       }
-    );
-  }
+      bcrypt.compare(req.body.password, user.password).then(
+        (valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              error: new Error('Incorrect password!')
+            });
+          }
+          const token = jwt.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' });
+          res.status(200).json({
+            userId: user._id,
+            token: token
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(500).json({
+            error: error
+          });
+        }
+      );
+    }
+  ).catch(
+    (error) => {
+      res.status(500).json({
+        error: error
+      });
+    }
+  );
+}
